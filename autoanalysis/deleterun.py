8ad786cae834bfd862dad29260b2d935ad2e2880
pajanne <pajanne@gmail.com>
2013-03-12 17:38:25 +0000

    python script for cleaning up run folder data on stagging servers - replacement of delete_old_run_data.pl

---
+++ autoanalysis/deleterun.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+deleterun.py
+
+$Id: $
+
+Created by Anne Pajon on 2013-03-08.
+"""
+
+################################################################################
+# IMPORTS
+################################################################################
+import sys
+import os
+import glob
+import logging
+import argparse
+import time
+
+try:
+    from sqlalchemy.ext.sqlsoup import SqlSoup
+    from sqlalchemy.orm.exc import NoResultFound
+except ImportError:
+    sys.exit("""
+--------------------------------------------------------------------------------
+>>> modules { mysql-python | sqlalchemy } not installed.
+--------------------------------------------------------------------------------
+[on sols] use /home/mib-cri/software/python2.7/bin/python
+[locally] install virtualenv; source bin/activate and pip install modules
+--------------------------------------------------------------------------------
+""")
+
+# import logging module first
+import log as logger
+log = logger.set_custom_logger()
+# then import other custom modules
+import utils
+import lims
+
+################################################################################
+# CONSTANTS
+################################################################################
+# Default filenames
+DONT_DELETE_FILENAME = 'dont.delete'
+SEQUENCING_COMPLETED_FILENAME = "Run.completed"
+THUMBNAILS_DELETED = 'Thumbnails.deleted'
+INTENSITIES_DELETED = 'Intensities.deleted'
+IMAGES_DELETED = 'Images.deleted'
+
+################################################################################
+# METHODS
+################################################################################
+def convert_day(day):
+    return 60*60*24*day
+    
+################################################################################
+# MAIN
+################################################################################
+
+# delete_old_run_data.pl -thumbnails 90 -images 14 -intensities 21 -runfolderlocglob '/solexa0*/data/Runs/??????_*'
+def main():
+    # get the options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--basedir", dest="basedir", action="store", help="run folder directories e.g. '/solexa0[1-8]/data/Runs'", required=True)
+    parser.add_argument("--thumbnails", dest="thumbnails", action="store", help="number of days to keep thumbnails - default set to 90", default=90, type=int)
+    parser.add_argument("--intensities", dest="intensities", action="store", help="number of days to keep intensities - default set to 21", default=21, type=int)
+    parser.add_argument("--images", dest="images", action="store", help="number of days to keep images - default set to 14", default=14, type=int)
+    parser.add_argument("--dburl", dest="dburl", action="store", default=lims.DB_URL, help="database url [read only access] - default set to '%s'" % lims.DB_URL)
+    parser.add_argument("--run", dest="run_number", action="store", help="run number e.g. '948'")
+    parser.add_argument("--dry-run", dest="dry_run", action="store_true", default=False, help="use this option to not do any shell command execution, only report actions")
+    parser.add_argument("--debug", dest="debug", action="store_true", default=False, help="Set logging level to DEBUG, by default INFO")
+    parser.add_argument("--logfile", dest="logfile", action="store", default=False, help="File to print logging information")
+
+    options = parser.parse_args()
+
+    # logging configuration
+    log.setLevel(logging.INFO)
+    if options.debug:
+        log.setLevel(logging.DEBUG)        
+    if options.logfile:
+        log.addHandler(logger.set_file_handler(options.logfile))
+                  
+    # lims db
+    solexa_db = SqlSoup(options.dburl)
+    
+    # setting-up time
+    present = time.time()
+    delete_thumbnails_older_than = convert_day(options.thumbnails)
+    delete_images_older_than = convert_day(options.images)
+    delete_intensities_older_than = convert_day(options.intensities)
+    
+    # removing data into completed run folders
+    run_folders = glob.glob("%s/??????_*_*_*" % options.basedir)
+    for run_folder in run_folders:
+        log.info('--------------------------------------------------------------------------------')
+        log.info('--- RUN: %s' % run_folder)
+        log.info('--------------------------------------------------------------------------------')
+        # check dont.delete is not present - stop cleaning if present
+        dont_delete = os.path.join(run_folder, DONT_DELETE_FILENAME)
+        if not os.path.exists(dont_delete):
+            try:
+                run = solexa_db.solexarun.filter_by(pipelinePath=os.path.basename(run_folder)).one()
+                log.info('Sequencing status %s and analysis status %s' % (run.status, run.analysisStatus))
+                if (run.status == 'COMPLETE' and (run.analysisStatus == 'COMPLETE' or run.analysisStatus == 'SECONDARY COMPLETE')) or ('ABORTED' in run.status) or (run.status == 'FAILED'):
+                    sequencing_completed = os.path.join(run_folder, SEQUENCING_COMPLETED_FILENAME)
+                    thumbnails_deleted = os.path.join(run_folder, THUMBNAILS_DELETED)
+                    intensities_deleted = os.path.join(run_folder, INTENSITIES_DELETED)
+                    images_deleted = os.path.join(run_folder, IMAGES_DELETED)
+                    if os.path.exists(sequencing_completed):
+                        sequencing_completed_date = os.path.getmtime(sequencing_completed)
+                        age = present - sequencing_completed_date
+                        log.info('[IMG:%s|INT:%s|PIC:%s] run completed %s' % (options.images, options.intensities, options.thumbnails, time.ctime(sequencing_completed_date)))
+                        if os.path.exists(images_deleted) and os.path.exists(intensities_deleted) and os.path.exists(thumbnails_deleted):
+                            log.info('All images/intensities/thumbnails deleted')
+                        if age > delete_images_older_than and not os.path.exists(images_deleted):
+                            delete_images_cmd = ['find', run_folder, '-name', '*.tif', '-delete']
+                            log.info('deleting images...')
+                            utils.touch(images_deleted)
+                            utils.run_bg_process(delete_images_cmd, options.dry_run)
+                        if age > delete_intensities_older_than and not os.path.exists(intensities_deleted):
+                            delete_intensities_cmd = ['find', '%s/Data/Intensities/' % run_folder, '-name', '*_pos.txt', 
+                            '-o', '-name', '*.cif', '-o', '-name', '*.filter', '-o', '-name', '*.bcl', '-o', '-name', '*.stats', '-delete']
+                            log.info('deleting intensities...')
+                            utils.touch(intensities_deleted)
+                            utils.run_bg_process(delete_intensities_cmd, options.dry_run)
+                        if age > delete_thumbnails_older_than and not os.path.exists(thumbnails_deleted):
+                            delete_thumbnails_cmd = ['find', '%s/Thumbnail_Images/' % run_folder, '-name', '*.jpg', '-delete']
+                            log.info('deleting thumbnails...')
+                            utils.touch(thumbnails_deleted)
+                            utils.run_bg_process(delete_thumbnails_cmd, options.dry_run)
+                    else:
+                        log.info('%s is missing' % sequencing_completed)
+            except (NoResultFound):
+                log.info('No result found in lims for pipelinePath %s' % os.path.basename(run_folder))
+            except:
+                log.exception("Unexpected error: ", sys.exc_info()[0])
+                raise
+        else:
+            log.debug('%s is present' % dont_delete)
+        
+if __name__ == '__main__':
+    main()
+
