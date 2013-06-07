5a20cfbd557c2ab1d49eecde6dc2a6f79976df69
pajanne <pajanne@gmail.com>
2013-06-07 10:35:13 +0000

    moved log configuration into dict to be able to handle external glsclient module. glslims has methods to create analysis processes and assign to publishing workflow

--- autoanalysis/autoanalysis.py
+++ autoanalysis/autoanalysis.py
@@ -25,10 +25,8 @@ import glob
 import logging
 import argparse
 
-# import logging module first
+# import custom modules
 import autoanalysis.log as logger
-log = logger.set_custom_logger()
-# then import other custom modules
 import autoanalysis.utils as utils
 import autoanalysis.runfolders as auto_runfolders
 import autoanalysis.pipelines as auto_pipelines
@@ -49,35 +47,34 @@ def main():
     parser.add_argument("--dry-run", dest="dry_run", action="store_true", default=False, help="use this option to not do any shell command execution, only report actions")
     parser.add_argument("--lims", dest="lims", action="store", default=auto_glslims.LIMS_SERVERS['dev'], help="lims servers to choose from %s - default set to %s" % (list(auto_glslims.LIMS_SERVERS.viewkeys()), auto_glslims.LIMS_SERVERS['dev']))
     parser.add_argument("--update-lims", dest="update_lims", action="store_true", default=False, help="use this option to update the lims")
-    parser.add_argument("--debug", dest="debug", action="store_true", default=False, help="Set logging level to DEBUG, by default INFO")
     parser.add_argument("--logfile", dest="logfile", action="store", default=False, help="File to print logging information")
 
     options = parser.parse_args()
 
     # logging configuration
-    log.setLevel(logging.INFO)
-    if options.debug:
-        log.setLevel(logging.DEBUG)        
+    log = logger.get_custom_logger()
     if options.logfile:
-        log.addHandler(logger.get_file_handler(options.logfile))
+        log = logger.get_custom_logger(options.logfile)
+    else:
+        log = logger.get_custom_logger()
                   
     try:
         # loop over all runs that have a Sequencing.completed file in options.basedir
-        runs = auto_runfolders.RunFolders(options.basedir, options.archivedir, options.run_folder, log)
+        runs = auto_runfolders.RunFolders(options.basedir, options.archivedir, options.run_folder)
         for run in runs.completed_runs:
             try:
                 log.info(run.getHeader())
                 # create pipelines
-                pipelines = auto_pipelines.Pipelines(run, options.step, options.softdir, options.cluster, options.dry_run, log)
+                pipelines = auto_pipelines.Pipelines(run, options.step, options.softdir, options.cluster, options.dry_run)
                 # execute pipelines
                 pipelines.execute()
                 # register completion
                 pipelines.registerCompletion()
                 if options.update_lims:
-                    glslims = auto_glslims.GlsLims(run.flowcell_id, options.lims)
-                    glslims.createAnalysisProcesses()
-                    glslims.publishFlowCell()
-                    glslims.updateSampleProgressStatus()
+                    glslims = auto_glslims.GlsLims(options.lims)
+                    glslims.createAnalysisProcesses(run.flowcell_id)
+                    #glslims.publishFlowCell()
+                    #glslims.updateSampleProgressStatus()
             except:
                 log.exception("Unexpected error")
                 continue
