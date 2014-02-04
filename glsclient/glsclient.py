88e2b3999714ca30abdb4126e272644dfe41944d
pajanne <pajanne@gmail.com>
2014-02-04 18:06:48 +0000

    remove some debug statements

--- glsclient/glsclient.py
+++ glsclient/glsclient.py
@@ -353,9 +353,8 @@ class GlsUtil(object):
         complete_run_process_byrunid = self.getLatestCompleteRunProcessByRunId(_run_id)
         # no complete run process found for run folder
         if complete_run_process_byrunid is None:
-            self.log.debug('no complete run process found v2')
+            self.log.debug('no complete run process found')
             run_process_byrunid = self.db.execute(glssql.PROCESS_BY_UDF_QUERY % ('%%Run%%', RUN_ID_FIELD, _run_id)).fetchall()
-            self.log.debug(run_process_byrunid)
             # no run process found for run folder
             if not run_process_byrunid:
                 self.log.debug('no run process found')
