c80c06fec131917046462c75e921ad25f0fa0d01
pajanne <pajanne@gmail.com>
2014-02-04 17:44:41 +0000

    add more debug log

--- glsclient/glsclient.py
+++ glsclient/glsclient.py
@@ -355,7 +355,9 @@ class GlsUtil(object):
         if complete_run_process_byrunid is None:
             self.log.debug('no complete run process found')
             self.log.debug(glssql.PROCESS_BY_UDF_QUERY % ('%%Run%%', RUN_ID_FIELD, _run_id))
+            self.log.debug('before run_process_byrunid query')
             run_process_byrunid = self.db.execute(glssql.PROCESS_BY_UDF_QUERY % ('%%Run%%', RUN_ID_FIELD, _run_id)).fetchall()
+            self.log.debug('after run_process_byrunid query')
             self.log.debug(run_process_byrunid)
             # no run process found for run folder
             if run_process_byrunid is None:
