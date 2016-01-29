7ef0ce290a57dde241c1a57716550e47895d9f7c
pajanne <pajanne@gmail.com>
2016-01-29 14:00:16 +0000

    add back HiSeq and Resubmit for HiSeq

--- autoanalysis/autoassignsamples.py
+++ autoanalysis/autoassignsamples.py
@@ -20,11 +20,13 @@ import glsclient.glsclient as glsclient
 import glsclient.glssql as glssql
 
 WORKFLOW_MAPPING = {
+'HiSeq': 'SLX: Sequencing for HiSeq v1',
 'HiSeq 2500': 'SLX: Sequencing for HiSeq v1',
 'HiSeq 2000': 'SLX: Sequencing for HiSeq 2000 v1',
 'HiSeq 4000': 'SLX: Sequencing for HiSeq 4000 v1',
 'HiSeq 2-lane': 'SLX: Sequencing for HiSeq Rapid Run v1',
 'HiSeq Express': 'SLX: Sequencing for HiSeq Express v1',
+'Resubmit for HiSeq': 'SLX: Resubmit for HiSeq v1',
 'Resubmit for HiSeq 2500': 'SLX: Resubmit for HiSeq v1',
 'Resubmit for HiSeq 4000': 'SLX: Resubmit for HiSeq v1',
 'MiSeq': 'SLX: Sequencing for MiSeq v1',
