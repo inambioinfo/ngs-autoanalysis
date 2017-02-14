2e7a507ef8f1403b9e9a03126a0735e1deb57f94
pajanne <pajanne@gmail.com>
2017-02-14 18:44:16 +0000

    Fix typo

--- cronscripts/run-autoanalysis.sh
+++ cronscripts/run-autoanalysis.sh
@@ -1,4 +1,4 @@
 AUTOANALYSIS_HOME=/home/mib-cri/software/ngs-autoanalysis/branch-2.0/
-source source /home/mib-cri/environment.bashrc.newservers
+source /home/mib-cri/environment.bashrc.newservers
 source $AUTOANALYSIS_HOME/venv/bin/activate
 python $AUTOANALYSIS_HOME/autoanalysis.py --clusterdir=/mnt/scratcha/bioinformatics/solexa/Runs/ --processingdir=/processing/ --stagingdir=/staging/ --softdir=/home/solexa/sequencingpipelines/ --cluster=clust1-headnode --logfile=/processing/Logs/autoanalysis.log
