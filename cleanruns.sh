609a08ecdc564021f8a562a8ba549bf36ed91e1f
pajanne <pajanne@gmail.com>
2017-04-25 15:39:50 +0100

    update scrpt to print out message when deleting run folders

--- cleanruns.sh
+++ cleanruns.sh
@@ -1,2 +1,2 @@
 SCRATCH_DIR=/mnt/scratcha/bioinformatics/solexa/Runs/
-find $SCRATCH_DIR/. -name pipeline.ended -mtime +5 | xargs -0 bash -c 'for f; do rm -rf $(dirname $(dirname $f)); done' bash
+find $SCRATCH_DIR/. -name pipeline.ended -mtime +5 | xargs -0 bash -c 'for f; do echo $(dirname $(dirname $f)) will be deleted; rm -rf $(dirname $(dirname $f)); done' bash
