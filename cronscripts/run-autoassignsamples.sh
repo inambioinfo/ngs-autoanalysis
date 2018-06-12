9e69ba761f40db57f36445756ef8b98b719deb0f
pajanne <pajanne@gmail.com>
2018-06-12 15:29:23 +0100

    move cron scripts to branch 2.1

--- cronscripts/run-autoassignsamples.sh
+++ cronscripts/run-autoassignsamples.sh
@@ -1,3 +1,3 @@
-AUTOANALYSIS_HOME=/home/mib-cri/software/ngs-autoanalysis/branch-2.0/
+AUTOANALYSIS_HOME=/home/mib-cri/software/ngs-autoanalysis/branch-2.1/
 source $AUTOANALYSIS_HOME/venv/bin/activate
 python $AUTOANALYSIS_HOME/autoassignsamples.py --logfile=/processing/Logs/autoassignsamples.log --update --updatesamples --email
--- cronscripts/run-manageglsevents.sh
+++ cronscripts/run-manageglsevents.sh
@@ -1,3 +1,3 @@
-AUTOANALYSIS_HOME=/home/mib-cri/software/ngs-autoanalysis/branch-2.0/
+AUTOANALYSIS_HOME=/home/mib-cri/software/ngs-autoanalysis/branch-2.1/
 source $AUTOANALYSIS_HOME/venv/bin/activate
 python $AUTOANALYSIS_HOME/manageglsevents.py --logfile=/processing/Logs/manageglsevents.log
