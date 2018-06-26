92baedb6fa08877d9f755462a153b857e2105f25
pajanne <pajanne@gmail.com>
2018-06-26 15:03:40 +0100

    add missing space on condition

--- cronscripts/run-manageglsevents.sh
+++ cronscripts/run-manageglsevents.sh
@@ -1,9 +1,5 @@
 AUTOANALYSIS_HOME=/home/mib-cri/software/ngs-autoanalysis/branch-2.2/
 PLATFORM=$(uname -r | cut -d '.' -f 6)
 source $AUTOANALYSIS_HOME/venv-$PLATFORM/bin/activate
-if ["$PLATFORM" == "el7"]; then
-  limsdev='--limsdev'
-else
-  limsdev=''
-fi
+if [ "$PLATFORM" == "el7" ]; then limsdev='--limsdev'; else limsdev=''; fi
 python $AUTOANALYSIS_HOME/manageglsevents.py --logfile=/processing/Logs/manageglsevents.log $limsdev
